d58bc6b4ebc949db3e62f1ad0d09094d05fdae47
Maximiliano <mbautistas011@gmail.com>
2024-02-23 17:37:22 -0600

    Refactor repository.

--- app/src/main/java/com/santanderhackaton/data/HarryPotterRepository.kt
+++ app/src/main/java/com/santanderhackaton/data/HarryPotterRepository.kt
@@ -1,32 +1,46 @@
 package com.santanderhackaton.data
 
-import com.santanderhackaton.data.network.HarryPotterService
+import com.santanderhackaton.data.network.HarryPotterApi
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class HarryPotterRepository @Inject constructor(
-    private val api: HarryPotterService
+    private val harryPotterApi: HarryPotterApi
 ) {
     suspend fun getAllCharacters(): Int {
-        return api.getAllCharacters()
+        return withContext(Dispatchers.IO) {
+            harryPotterApi.getAllCharacters().code()
+        }
     }
 
     suspend fun getCharacter(): Int {
-        return api.getCharacter()
+        return withContext(Dispatchers.IO) {
+            harryPotterApi.getCharacter().code()
+        }
     }
 
     suspend fun getHogwartsStudents(): Int {
-        return api.getHogwartsStudents()
+        return withContext(Dispatchers.IO) {
+            harryPotterApi.getHogwartsStudents().code()
+        }
     }
 
     suspend fun getHogwartsStaff(): Int {
-        return api.getHogwartsStaff()
+        return withContext(Dispatchers.IO) {
+            harryPotterApi.getStaffHogwartsStaff().code()
+        }
     }
 
     suspend fun getCharactersInHouse(): Int {
-        return api.getCharactersInHouse()
+        return withContext(Dispatchers.IO) {
+            harryPotterApi.getCharactersInHouse().code()
+        }
     }
 
     suspend fun getAllSpells(): Int {
-        return api.getAllSpells()
+        return withContext(Dispatchers.IO) {
+            harryPotterApi.getSpells().code()
+        }
     }
 }
